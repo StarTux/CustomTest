05af5c70ec8fcc3ab77750be281c342a3c76c9d7
Thomas Loy <ths.loy@googlemail.com>
2016-12-14 00:42:59 +0100

    Update with new features

--- src/main/java/com/winthier/customtest/TestCow.java
+++ src/main/java/com/winthier/customtest/TestCow.java
@@ -1,10 +1,14 @@
 package com.winthier.customtest;
 
 import com.winthier.custom.CustomConfig;
+import com.winthier.custom.entity.CustomEntity;
 import com.winthier.custom.entity.DefaultCustomEntity;
-import com.winthier.custom.event.EntityEventContext;
+import com.winthier.custom.entity.DefaultEntityWatcher;
+import com.winthier.custom.entity.EntityContext;
+import com.winthier.custom.entity.EntityWatcher;
 import org.bukkit.Location;
 import org.bukkit.Sound;
+import org.bukkit.entity.Cow;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.EntityType;
 import org.bukkit.event.EventHandler;
@@ -18,15 +22,39 @@ public class TestCow extends DefaultCustomEntity {
 
     @Override
     public Entity spawnEntity(Location loc, CustomConfig config) {
-        return loc.getWorld().spawnEntity(loc, EntityType.COW);
+        return loc.getWorld().spawn(loc, Cow.class);
     }
 
-    @EventHandler
-    public void onEntityDamage(EntityDamageByEntityEvent event) {
-        event.setCancelled(true);
-        EntityEventContext context = EntityEventContext.of(event);
-        if (context.getPosition() != EntityEventContext.Position.ENTITY) return;
-        Entity entity = event.getEntity();
-        entity.getWorld().playSound(entity.getLocation(), Sound.ENTITY_CAT_AMBIENT, 1.0f, 1.0f);
+    @Override
+    public EntityWatcher createEntityWatcher(final Entity entity, final CustomConfig config) {
+        Cow cow = (Cow)entity;
+        cow.setCustomName("Test");
+        return new TestCowWatcher(entity, this, config);
     }
+
+    
+    public static class TestCowWatcher extends DefaultEntityWatcher {
+        TestCowWatcher(Entity entity, CustomEntity customEntity, CustomConfig config) {
+            super(entity, customEntity, config);
+        }
+        
+        @EventHandler
+        public void onEntityDamageByEntity(EntityDamageByEntityEvent event) {
+            event.setCancelled(true);
+            EntityContext context = EntityContext.of(event);
+            if (context.getPosition() != EntityContext.Position.ENTITY) return;
+            Entity entity = event.getEntity();
+            entity.getWorld().playSound(entity.getLocation(), Sound.ENTITY_CAT_AMBIENT, 1.0f, 1.0f);
+        }
+
+        @Override
+        public void didDiscoverEntity() {
+            System.out.println("Test Cow reporting for duty. Moo!");
+        }
+
+        @Override
+        public void willUnloadEntity() {
+            System.out.println("Test Cow over and out!");
+        }
+    };
 }
--- src/main/java/com/winthier/customtest/TestItem.java
+++ src/main/java/com/winthier/customtest/TestItem.java
@@ -2,19 +2,22 @@ package com.winthier.customtest;
 
 import com.winthier.custom.CustomConfig;
 import com.winthier.custom.CustomPlugin;
-import com.winthier.custom.event.ItemEventContext;
 import com.winthier.custom.item.CustomItem;
+import com.winthier.custom.item.DefaultCustomItem;
+import com.winthier.custom.item.ItemContext;
 import lombok.Getter;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.Particle;
 import org.bukkit.Sound;
 import org.bukkit.block.Block;
+import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.Event;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.EntityShootBowEvent;
+import org.bukkit.event.player.PlayerFishEvent;
 import org.bukkit.event.player.PlayerInteractEntityEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.event.player.PlayerSwapHandItemsEvent;
@@ -27,39 +30,47 @@ public class TestItem implements CustomItem {
 
     @Override
     public ItemStack spawnItemStack(int amount, CustomConfig config) {
-        return new ItemStack(Material.BOW, amount);
+        return new ItemStack(Material.FISHING_ROD, amount);
     }
     
-    @EventHandler
-    public void handlePlayerInteract(PlayerInteractEvent event) {
-        if (event.isCancelled()) return;
-        if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
-            Block block = event.getClickedBlock().getRelative(event.getBlockFace());
-            Location loc = block.getLocation().add(0.5, 0.5, 0.5);
-            block.getWorld().spawnParticle(Particle.ENCHANTMENT_TABLE, loc.getX(), loc.getY(), loc.getZ(), 32);
-        }
-        event.setCancelled(true);
-    }
+    // @EventHandler
+    // public void handlePlayerInteract(PlayerInteractEvent event) {
+    //     if (event.isCancelled()) return;
+    //     if (event.getAction() == Action.RIGHT_CLICK_BLOCK) {
+    //         Block block = event.getClickedBlock().getRelative(event.getBlockFace());
+    //         Location loc = block.getLocation().add(0.5, 0.5, 0.5);
+    //         block.getWorld().spawnParticle(Particle.ENCHANTMENT_TABLE, loc.getX(), loc.getY(), loc.getZ(), 32);
+    //     }
+    //     event.setCancelled(true);
+    // }
 
-    @EventHandler
-    public void handlePlayerInteractEntity(PlayerInteractEntityEvent event) {
-        if (event.isCancelled()) return;
-        Location loc = event.getRightClicked().getLocation();
-        event.getRightClicked().getWorld().spawnParticle(Particle.ENCHANTMENT_TABLE, loc.getX(), loc.getY(), loc.getZ(), 32);
-        event.setCancelled(true);
-    }
+    // @EventHandler
+    // public void handlePlayerInteractEntity(PlayerInteractEntityEvent event) {
+    //     if (event.isCancelled()) return;
+    //     Location loc = event.getRightClicked().getLocation();
+    //     event.getRightClicked().getWorld().spawnParticle(Particle.ENCHANTMENT_TABLE, loc.getX(), loc.getY(), loc.getZ(), 32);
+    //     event.setCancelled(true);
+    // }
 
-    @EventHandler
-    public void onEntityShootBow(EntityShootBowEvent event) {
-        Player player = (Player)event.getEntity();
-        player.playSound(player.getEyeLocation(), Sound.ENTITY_CAT_AMBIENT, 1.0f, 1.0f);
-    }
+    // @EventHandler
+    // public void onEntityShootBow(EntityShootBowEvent event) {
+    //     Player player = (Player)event.getEntity();
+    //     player.playSound(player.getEyeLocation(), Sound.ENTITY_CAT_AMBIENT, 1.0f, 1.0f);
+    // }
 
     @EventHandler
     public void onPlayerSwapHandItems(PlayerSwapHandItemsEvent event) {
-        ItemEventContext context = ItemEventContext.of(event);
-        if (context.getHand() != EquipmentSlot.HAND) return;
+        ItemContext context = ItemContext.of(event);
+        if (context.getPosition() != ItemContext.Position.HAND) return;
         event.setCancelled(true);
         context.getPlayer().sendMessage("BOOM");
+    }
+
+    @EventHandler
+    public void onPlayerFish(PlayerFishEvent event) {
+        event.getPlayer().sendMessage("Test Fish");
+        if (event.getState() == PlayerFishEvent.State.CAUGHT_FISH) {
+            event.getPlayer().getWorld().spawnEntity(event.getHook().getLocation(), EntityType.CREEPER);
+        }
     }
 }
